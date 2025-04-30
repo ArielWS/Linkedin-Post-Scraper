688cf6bc83b1c2dec8de79caa9977ec00f08b5a0
Ariel Wertlen Spilkin <arielws1993@gmail.com>
2025-04-30 09:45:14 +0200

    update web broswer

--- linkedin-post-scraper.py
+++ linkedin-post-scraper.py
@@ -1,14 +1,13 @@
 # Import necessary libraries
-from selenium import webdriver
+import undetected_chromedriver as uc
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options  # you may not need this if you only use uc.ChromeOptions
 from bs4 import BeautifulSoup as bs
-import time
-import pandas as pd
-import re
+import time, re, pandas as pd
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
+from dateutil.relativedelta import relativedel
 
 # Initialize Chrome options
 chrome_options = Options()
